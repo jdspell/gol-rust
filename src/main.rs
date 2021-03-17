da3c2fa17efb917270861ed1114ae69ccbb2fbc9
jdspell <spelljd15@gmail.com>
2021-03-16 20:59:21 -0400

    setup up initial pts with neighbors list

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,31 @@
 use std::collections::HashMap;
+use std::str;
 
-// struct Point {
-//     x: i32,
-//     y: i32,
-// }
-struct Point (f32, f32);
+#[derive(Hash, Eq, PartialEq, Debug)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Point {
+    // creates new Point
+    fn new(x: i32, y: i32) -> Point {
+        Point { x: x, y: y}
+    }
+}
 
 fn main() {
-    // get, remove, insert
-    let mut current = HashMap::<Point, i32>::new();
+    // stores the points with neighbors
+    let mut ptsWitNeighbors = HashMap::new();
 
-    // user needs to pass in a list of points to start
-    // let ptOne: Point = Point { x: 0, y: 0 };
-    // let ptTwo: Point = Point { x: 1, y: 0 };
-    // let ptThree: Point = Point { x: 0, y: 1 };
-    // let ptFour: Point = Point { x: 1, y: 1 };
-    let ptOne: Point = Point(0, 0);
-    let ptOne: Point = Point(1, 0);
-    let ptOne: Point = Point(0, 1);
-    let ptOne: Point = Point(1, 1);
-
-    current.insert(ptOne, 1);
-    current.insert(ptTwo, 2);
-    current.insert(ptThree, 3);
-    current.insert(ptFour, 4);
+    // initial list of points to start
+    ptsWitNeighbors.insert(Point::new(0, 0), 1);
+    ptsWitNeighbors.insert(Point::new(1, 0), 2);
+    ptsWitNeighbors.insert(Point::new(0, 1), 3);
+    ptsWitNeighbors.insert(Point::new(1, 1), 4);
 
     // add the points to the hashmap
-    for (pt, neighbors) in current.iter() {
-        println!("({}, {}) -> {} neighbors", pt.0, pt.1, neighbors);
+    for (pt, neighbors) in ptsWitNeighbors.iter() {
+        println!("({}, {}) -> {} neighbors", pt.x, pt.y, neighbors);
     }
 }
