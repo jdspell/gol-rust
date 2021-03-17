eee50c162c9fae79f23e26aa0ee3b028d7b82371
jdspell <spelljd15@gmail.com>
2021-03-16 20:35:59 -0400

    added initial pts to hashmap representing game state

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,32 @@
 use std::collections::HashMap;
-// use array_macro::array;
 
-struct Point {
-    x: i32,
-    y: i32,
-}
-
+// struct Point {
+//     x: i32,
+//     y: i32,
+// }
+struct Point (f32, f32);
 
 fn main() {
     // get, remove, insert
     let mut current = HashMap::<Point, i32>::new();
 
     // user needs to pass in a list of points to start
-    let initialPts: [[Point; 4]] = [
-        Point { x: 0, y: 0 },
-        Point { x: 1, y: 0 },
-        Point { x: 0, y: 1 },
-        Point { x: 1, y: 1 },
-    ];
+    // let ptOne: Point = Point { x: 0, y: 0 };
+    // let ptTwo: Point = Point { x: 1, y: 0 };
+    // let ptThree: Point = Point { x: 0, y: 1 };
+    // let ptFour: Point = Point { x: 1, y: 1 };
+    let ptOne: Point = Point(0, 0);
+    let ptOne: Point = Point(1, 0);
+    let ptOne: Point = Point(0, 1);
+    let ptOne: Point = Point(1, 1);
+
+    current.insert(ptOne, 1);
+    current.insert(ptTwo, 2);
+    current.insert(ptThree, 3);
+    current.insert(ptFour, 4);
 
     // add the points to the hashmap
-    for pt in initialPts.iter() {
-        println!("({}, {})", pt.x, pt.y);
-
+    for (pt, neighbors) in current.iter() {
+        println!("({}, {}) -> {} neighbors", pt.0, pt.1, neighbors);
     }
-    println!();
 }
